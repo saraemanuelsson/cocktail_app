6da59ce11c6f7fa36027c0ce8878432d22cf3a78
Sara Emanuelsson <sara256emanuelsson@gmail.com>
2020-06-27 14:29:46 +0100

    Added elements for cocktail pic and name

---
+++ src/components/CocktailDetail.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="drink-recipe">
+      <cocktail-name></cocktail-name>
+      <cocktail-image></cocktail-image>
+      <p>Ingredients</p>
+      <p>Method</p>
+  </div>
+</template>
+
+<script>
+
+
+export default {
+
+}
+</script>
+
+<style>
+
+.drink-recipe {
+    display: grid;
+    grid-template-columns: 1fr 1fr 1fr 1fr 1fr;
+}
+
+</style>
